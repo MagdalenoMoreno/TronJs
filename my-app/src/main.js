a76946d8bc2ed36df0ed3fc3464bee3e8e040cb1
MagdalenoMoreno <adriamorriocollado@gmail.com>
2025-10-11 01:42:41 +0200

    Movimiento de las motos configurado

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,45 +1,43 @@
 import './style.css'
 
+const EstelaAzul = []
+
+const EstelaRoja = []
+
 const MotoAzul = {
   img: new Image(),
   x: 450,
-  y: 301,
+  y: 318,
   ancho: 15,
   alto: 35,
   angulo: 0,
   lastMove: "arriba",
-  estado: "vivo"
+  estado: "vivo",
+  estela: EstelaAzul,
+  velocidad: 2
   
 }
 
 const MotoRoja = {
   img: new Image(),
   x: 150,
-  y: 265,
+  y: 282,
   ancho: 15,
   alto: 35,
   angulo: 0,
   lastMove: "abajo",
-  estado: "vivo"
-  
+  estado: "vivo",
+  estela: EstelaRoja,
+  velocidad: 2
 }
 
-const EstelaAzul = []
-
-const EstelaRoja = []
 
 inicialitzar();
 
 function inicialitzar() {
-  
-  
-  for (let i = 0; i < 20; i++) {
-    console.log(MotoAzul.estado);
     tableroInicial();
-    mover();
+    setInterval(RehacerTablero, 1000 / 360);
     
-  }
-  
 }
 
 
@@ -57,22 +55,23 @@ function tableroInicial() {
   celda.fillStyle = 'rgba(255, 255, 255, 1)';
   celda.fillRect(3, 300, 10000, 1);
   
+let cargadas = 0;
+  function imagenCargada() {
+    cargadas++;
+    if (cargadas === 2) {
+      RehacerTablero();
+    }
+  }
+
+  MotoAzul.img.onload = imagenCargada;
+  MotoRoja.img.onload = imagenCargada;
 
   MotoAzul.img.src = "img/motoAzul.png";
-  MotoAzul.img.onload = function() {
-    celda.drawImage(MotoAzul.img, MotoAzul.x, MotoAzul.y, 15, 35);
-
-  }
-
   MotoRoja.img.src = "img/motoRoja.png";
-  MotoRoja.img.onload = function() {
-    celda.drawImage(MotoRoja.img, MotoRoja.x, MotoRoja.y, 15, 35);
-
-  }
   
 }
 
-function RehacerTableroAzul(num) {
+function RehacerTablero() {
   const canvas = document.getElementById("tablero");
     const celda = canvas.getContext('2d');
     
@@ -84,102 +83,100 @@ function RehacerTableroAzul(num) {
     celda.fillStyle = 'rgba(255, 255, 255, 1)';
     celda.fillRect(3, 300, 10000, 1);
 
-    celda.save();
-    celda.translate(MotoRoja.x, MotoRoja.y);
-    celda.rotate(MotoRoja.angulo);
-    celda.drawImage(MotoRoja.img, -MotoRoja.ancho / 2, -MotoRoja.alto / 2, MotoRoja.ancho, MotoRoja.alto);
-    celda.restore();
+    mover();
 
-    celda.save();
-    celda.translate(MotoAzul.x, MotoAzul.y);
-    celda.rotate(MotoAzul.angulo = num * Math.PI / 180);
-    celda.drawImage(MotoAzul.img,-MotoAzul.ancho / 2, -MotoAzul.alto / 2, MotoAzul.ancho, MotoAzul.alto);
-    celda.restore();
+    if (MotoAzul.estado !== "vivo" || MotoRoja.estado !== "vivo") {
+      console.log("Game Over");
+    } else {
+
+      celda.save();
+      celda.translate(MotoRoja.x, MotoRoja.y);
+      celda.rotate(MotoRoja.angulo);
+      celda.drawImage(MotoRoja.img, -MotoRoja.ancho / 2, -MotoRoja.alto / 2, MotoRoja.ancho, MotoRoja.alto);
+      celda.restore();
+      
+      celda.save();
+      celda.translate(MotoAzul.x, MotoAzul.y);
+      celda.rotate(MotoAzul.angulo);
+      celda.drawImage(MotoAzul.img,-MotoAzul.ancho / 2, -MotoAzul.alto / 2, MotoAzul.ancho, MotoAzul.alto);
+      celda.restore();
+    }
 }
-
-function RehacerTableroRojo(num) {
-  const canvas = document.getElementById("tablero");
-    const celda = canvas.getContext('2d');
-    
-    celda.clearRect(0, 0, canvas.width, canvas.height);
-    
-    celda.fillStyle = "rgb(43, 43, 43)";
-    celda.fillRect(0, 0, 600, 600)
-
-    celda.fillStyle = 'rgba(255, 255, 255, 1)';
-    celda.fillRect(3, 300, 10000, 1);
-
-    celda.save();
-    celda.translate(MotoAzul.x, MotoAzul.y);
-    celda.rotate(MotoAzul.angulo);
-    celda.drawImage(MotoAzul.img,-MotoAzul.ancho / 2, -MotoAzul.alto / 2, MotoAzul.ancho, MotoAzul.alto);
-    celda.restore();
-
-    celda.save();
-    celda.translate(MotoRoja.x, MotoRoja.y);
-    celda.rotate(MotoRoja.angulo = num * Math.PI / 180);
-    celda.drawImage(MotoRoja.img,-MotoRoja.ancho / 2, -MotoRoja.alto / 2, MotoRoja.ancho, MotoRoja.alto);
-    celda.restore();
-}
-
 
 document.addEventListener("keydown", function(event) {
   switch (event.key) {
     case "ArrowUp": 
       if (MotoAzul.lastMove !== "arriba" && MotoAzul.lastMove !== "abajo") {
-        moverArribaAzul();
         MotoAzul.lastMove = "arriba";
+        MotoAzul.y -= MotoAzul.velocidad;
+        MotoAzul.angulo = 0;
+        RehacerTablero();
       }
 
       break;
 
     case "ArrowDown":
       if (MotoAzul.lastMove !== "arriba" && MotoAzul.lastMove !== "abajo") {
-        moverAbajoAzul();
         MotoAzul.lastMove = "abajo";
+        MotoAzul.y += MotoAzul.velocidad;
+        MotoAzul.angulo = Math.PI;
+        RehacerTablero();
       }
 
       break;
 
     case "ArrowLeft":
       if (MotoAzul.lastMove !== "izquierda" && MotoAzul.lastMove !== "derecha") {
-        moverIzquierdaAzul();
         MotoAzul.lastMove = "izquierda";
+        MotoAzul.x -= MotoAzul.velocidad;
+        MotoAzul.angulo = 270 * Math.PI / 180;
+        RehacerTablero();
       }
       break;
 
     case "ArrowRight": 
       if (MotoAzul.lastMove !== "izquierda" && MotoAzul.lastMove !== "derecha") {
-        moverDerechaAzul();
         MotoAzul.lastMove = "derecha";
+        MotoAzul.x += MotoAzul.velocidad;
+        MotoAzul.angulo = 90 * Math.PI / 180;
+        RehacerTablero();
       }
       break;
 
     case "w":
       if (MotoRoja.lastMove !== "arriba" && MotoRoja.lastMove !== "abajo") {
-        moverArribaRojo();
         MotoRoja.lastMove = "arriba";
-      }
-      break;
-
-    case "a": 
-      if (MotoRoja.lastMove !== "derecha" && MotoRoja.lastMove !== "izquierda") {
-        moverIzquierdaRojo();
-        MotoRoja.lastMove = "izquierda";
+        MotoRoja.y -= MotoRoja.velocidad;
+        MotoRoja.angulo = Math.PI;
+        RehacerTablero();
       }
       break;
 
     case "s": 
       if (MotoRoja.lastMove !== "arriba" && MotoRoja.lastMove !== "abajo") {
-        moverAbajoRojo();
         MotoRoja.lastMove = "abajo";
+        MotoRoja.y += MotoRoja.velocidad;
+        MotoRoja.angulo = 0;
+        RehacerTablero();
       }
       break;
 
+    case "a": 
+      if (MotoRoja.lastMove !== "derecha" && MotoRoja.lastMove !== "izquierda") {
+        MotoRoja.lastMove = "izquierda";
+        MotoRoja.x -= MotoRoja.velocidad;
+        MotoRoja.angulo = 90 * Math.PI / 180;
+        RehacerTablero();
+      }
+      break;
+
+
     case "d":
       if (MotoRoja.lastMove !== "derecha" && MotoRoja.lastMove !== "izquierda") {
-        moverDerechaRojo();
         MotoRoja.lastMove = "derecha";
+        MotoRoja.x += MotoRoja.velocidad;
+        MotoRoja.angulo = 270 * Math.PI / 180;
+        RehacerTablero();
       }
       break;
 
@@ -191,72 +188,36 @@ document.addEventListener("keydown", function(event) {
 function mover() {
     correrAzul();
     correrRojo();
-
-
 }
 
 function correrRojo() {
   switch (MotoRoja.lastMove) {
-    case "arriba": MotoRoja.y -= 10 
+    case "arriba": MotoRoja.y -= MotoRoja.velocidad;
       break;
 
-    case "izquierda": MotoRoja.x -= 10
+    case "izquierda": MotoRoja.x -= MotoRoja.velocidad;
       break;
 
-    case "derecha": MotoRoja.x += 10
+    case "derecha": MotoRoja.x += MotoRoja.velocidad;
       break;
 
-    case "abajo": MotoRoja.y += 10 
+    case "abajo": MotoRoja.y += MotoRoja.velocidad;
       break; 
   }
 }
 
 function correrAzul() {
   switch (MotoAzul.lastMove) {
-    case "arriba": MotoAzul.y -= 10 
+    case "arriba": MotoAzul.y -= MotoAzul.velocidad;
       break;
 
-    case "izquierda": MotoAzul.x -= 10
+    case "izquierda": MotoAzul.x -= MotoAzul.velocidad;
       break;
 
-    case "derecha": MotoAzul.x += 10
+    case "derecha": MotoAzul.x += MotoAzul.velocidad;
       break;
 
-    case "abajo": MotoAzul.y += 10 
+    case "abajo": MotoAzul.y += MotoAzul.velocidad;
       break; 
   }
-}
-
-function moverArribaAzul() {
-  RehacerTableroAzul(360);
-
-}
-
-function moverAbajoAzul() {
-  RehacerTableroAzul(180);
-}
-
-function moverIzquierdaAzul() {
-  RehacerTableroAzul(270);
-}
-
-function moverDerechaAzul() {
-  RehacerTableroAzul(90);
-}
-
-function moverArribaRojo() {
-  RehacerTableroRojo(180);
-}
-
-function moverAbajoRojo() {
-  RehacerTableroRojo(0);
-}
-
-function moverIzquierdaRojo() {
-    RehacerTableroRojo(90);
-
-}
-
-function moverDerechaRojo() {
-  RehacerTableroRojo(270);
 }
